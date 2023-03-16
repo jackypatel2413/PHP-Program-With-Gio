0b5aa38e09b027cc41fbfe5bd6988fc268d419c1
jackypatel2413 <jackypatel2413@gmail.comm>
2023-03-16 16:51:39 +0530

    video 16

---
+++ video16.php
@@ -0,0 +1,36 @@
+<?php
+
+/* Control Structures */
+// -- If -- //
+
+$a= 95;
+if ($a >90) {
+    echo 'A';
+}
+echo'<hr>';
+
+// -- Else -- //
+$b= 85;
+if ($b >90) {
+    echo 'A';
+}else {
+    echo 'B';
+}
+echo'<hr>';
+
+// -- Elseif -- //
+$c= 85;
+if ($c > 90) {
+    echo 'A';
+} elseif($c > 80) {
+    echo 'B';
+} elseif ($c > 70) {
+    echo 'D';
+} else {
+    echo 'E';
+}
+echo'<hr>';
+
+
+
+?>
