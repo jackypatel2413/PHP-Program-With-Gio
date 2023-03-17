ba21ba4bd2fe55bfdce3905266e0db3f0c6a9c86
jackypatel2413 <jackypatel2413@gmail.comm>
2023-03-17 15:28:37 +0530

    video 21

---
+++ file1.php
@@ -0,0 +1,5 @@
+<?php
+
+echo 'Hello from File 1. <br>';
+
+?>
---
+++ video21.php
@@ -0,0 +1,20 @@
+<?php
+
+/* Require / Require_Once */
+// -- Require -- //
+echo 'Require : <br><br>';
+require('file1.php');
+require('file1.php');
+echo 'Hello World';
+echo '<hr>';
+
+// -- Require_Once -- //
+echo 'Require_once : <br><br>';
+require_once('file2.php');
+require_once('file2.php');
+echo 'Hello World';
+echo '<hr>';
+
+
+
+?>
